4841b3f1fe6db087366c706f32734a7a349cfc05
martinim <you@example.com>
2020-11-25 13:46:55 +0300

    final commit 25/11/20

--- catalog/forms.py
+++ catalog/forms.py
@@ -9,26 +9,9 @@ from functools import partial
 DateInput = partial(forms.DateInput, {'class': 'datepicker'})
     
 class CarsForm(forms.ModelForm):
-    # date_on = forms.DateTimeField(
-    #     input_formats=['%Y/%m/%d'],
-    #     widget=forms.DateInput(attrs={
-    #         # 'data-target': '#datetimepicker1'
-    #     })
-    # )
-    # date_off = forms.DateTimeField(
-    #     input_formats=['%d/%m/%Y %H:%M'],
-    #     widget=forms.DateTimeInput(attrs={
-    #         'class': 'form-control datetimepicker-input',
-    #         'data-target': '#datetimepicker2'
-    #     })
-    # )
-
-    # date_on = forms.DateField(input_formats=['%d/%m/%Y'])
     class Meta:
         model = Cars
         fields = ('name', 'date_on','date_off')
-    # renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
-
     def clean_renewal_date(self):
         data = self.cleaned_data['renewal_date']
         
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 from django.utils import timezone, dateformat
-# Create your views here.
 from .models import Cars
-# from .models import CarsInstance
 from .forms import CarsForm
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
+
     num_cars=Cars.objects.all().count()
     name_cars=Cars.objects.all()
     
@@ -36,24 +31,8 @@ from django.utils.formats import get_format
 def cars_filter(request):
     
     if request.method == "POST":
-        
-        # name_cars=Cars.objects.all()
         form = CarsForm(request.POST)
-        # d_on = request.POST['date_on']
-        # d_off = request.POST['date_off']
-        # d_on1 = DateFormat(d_on)
-        # d_off2 = DateFormat(d_off)
-        # d_on1.format('Y-m-d')
-        # df2.format('Y-m-d')
         name_cars = Cars.objects.filter(date_on__isnull=True, date_on__range=(request.POST['date_on'], request.POST['date_off']))
-
-        # cars = CarsForm(request.POST)
-        # cars.date_on = timezone.now()
-        # cars.name =  request.user
-
-        # cars.save()
-
-        # return HttpResponseRedirect(reverse('cars') )
         return render(
         request,
         'catalog/cars_filter.html',
@@ -77,13 +56,9 @@ def cars_new(request):
     if request.method == "POST":
         form = CarsForm(request.POST)
         cars = form.save(commit=False)
-        # cars.date_on = request.POST['date_on']
-        # cars.name = request.user
-
         cars.save()
 
         return HttpResponseRedirect(reverse('cars') )
-		# return redirect ('catalog/cars_edit.html', pk=post.pk)
     else:
         form = CarsForm()
     return render(request, 'catalog/cars_edit.html', {'form': form })
@@ -93,9 +68,6 @@ def cars_edit(request, pk):
     if request.method == "POST":
         form = CarsForm(request.POST, instance=post)
         if form.is_valid():
-            # post = form.save(commit=False)
-            # post.name = request.name
-            # post.published_date = timezone.now()
             post.save()
             return HttpResponseRedirect(reverse('cars') )
     else:
